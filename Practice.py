29ffd82c9dcfcb4e17289c3c07c804e20f6a9c46
Chetan Chauhan <chetanchauhan247@gmail.com>
2026-03-01 11:12:48 +0530

    python

--- Practice.py
+++ Practice.py
@@ -13,11 +13,86 @@
 #     a=a//10
 # print(count)
 
-a=123
-s=0
+# a=123
+# s=0
 
-while a%10!=0:
-    r=a%10
-    s= s * 10 + r
-    a=a//10
-print(s)
+# while a%10!=0:
+#     r=a%10
+#     s= s * 10 + r
+#     a=a//10
+# print(s)
+
+
+# a=123
+# s=0
+
+# while a%10!=0:
+#     l=a%10
+#     s=s*10+l
+#     a=a//10
+# print(s)
+
+# while s!=0:
+#     r=s%10
+#     print(r)
+#     s=s//10
+
+# a=121
+# n=a
+# s=0
+# while a%10!=0:
+#     l=a%10
+#     s=s*10+l
+#     a=a//10
+# print(s)
+ 
+# if(s==n):
+#     print("palindrome")
+# else:
+#     print("Not")
+
+# a=123
+# sum=0
+# while a%10!=0:
+#     l=a%10
+#     sum+=l
+#     a=a//10
+# print(sum)
+
+# a=153
+# sum=0
+# num=a
+
+# while a%10!=0:
+#     l=a%10
+#     sum=sum+l**3
+#     a=a//10
+# print(sum)
+
+# if(sum==num):
+#     print("Armstrong")
+# else:
+#     print("Not a armstrong")
+
+# a=2123
+# t=2
+# count=0
+
+# while a%10!=0:
+#     l=a%10
+#     if(l==t):
+#         count=count+1
+#     a=a//10
+# print(count)
+
+# a=123
+
+# while a%10!=0:
+#     l=a%10
+#     if(l%2==0):
+#         print("Ever")
+#     else:
+#         print("Odd")
+#     a=a//10
+
+
---
+++ for.py
@@ -0,0 +1,81 @@
+# x="string"
+# for x in "india":
+#     print(x)
+
+# for x in range(1,11):
+#     print(x)
+
+# for x in range(1,11,2):
+#     print(x)
+
+# for x in range(10,0,-1):
+#     print(x)
+
+# n=4
+# sum=0
+# for x in range(0,n+1,1):
+#     sum=sum+x
+#     print(sum)
+
+# n=4
+# fac=1
+# for x in range(1,n+1,1):
+#     fac=fac*x
+#     print(fac)
+
+
+
+# n=4
+# sum=1
+# a=0
+# b=1
+
+# # print(" " ,a," ",b,end="")
+# for i in range(1,n):
+#     c=a+b
+#     # print(" ",c, end="")
+#     a=b
+#     b=c
+# # print(c)
+
+
+# ls=[10,20,30,40,50]
+# # for i in  ls:
+#     # print(ls[2])
+# print(ls[2])
+
+# for index in range(0,len(ls),2):
+#     print(ls)
+
+
+# n=7
+# s=0
+# ls=[2,-3,8,-6,-7,18,1]
+# for index in range(0,len(ls),2):
+#     s=s+ls[index]
+
+# print(s)
+
+
+# size=int(input())
+# req=list(map(int,input().split()))
+# print(req)
+
+def memServer1(req):
+     
+    s=0
+    for index in range(0,len(req),2):
+    s = s + req[index]
+
+    return
+
+def main():
+    req=[]
+    size=int(input())
+    req=list(map(int,input().split()))
+
+    result = memServer1(req)
+    print(result)
+
+if__name__=="__main__":
+    main()
