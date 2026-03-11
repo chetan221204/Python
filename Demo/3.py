6a51d90b77151843093d39bbe36a74445ffe7aaf
Chetan Chauhan <chetanchauhan247@gmail.com>
2026-03-11 23:36:10 +0530

    pattern

---
+++ Demo/3.py
@@ -0,0 +1,136 @@
+# Nested For Loop
+# for i in range(3):
+#     for j in range(2):
+#         print(i, j)
+
+
+# # Q1
+# n=5
+# for i in range(0,n):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()
+
+
+# # Q2
+# n=4
+# for i in range(n):
+#     for j in range(i+1):
+#         print(i,end="")
+#     print()
+
+# # Q3
+# n=4
+# for i in range(n):
+#     for j in range(i+1):
+#         print(n,end="")
+#     print()
+
+# Q4
+# n=3
+# t=1
+# for i in range(n):
+#     for j in range(i+1):
+#         print(t,end=" ")
+#         t=t+1
+#     print()
+
+# Q5
+# n=3
+# t=65
+# for i in range(n):
+#     for j in range(i+1):
+#         print( chr(t),end=" ")
+#         t=t+1
+#     print()
+
+
+# # Q6
+# n=3
+# t=65
+# for i in range(n):
+#     for j in range(i+1):
+#         print( chr(t),end=" ")
+#         t=t+1
+#     print()
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# n=3
+# for i in range(3):
+#     for j in range(3):
+#         print("*",end="")
+#     print()
+
+# n=4
+# for i in range(n):
+#     for j in range(i):
+#         print("*",end="")
+#     print("")
+
+# n=4
+# for i in range (n):
+#     for j in range(n-i):
+#         print("*",end="")
+#     print()
+
+# n=5
+# for i in range(n):
+#     for j in range(n-i):
+#         print(" ",end="")
+#     # print()
+ 
+#     for k in range(i):
+#         print("*",end="")
+#     for l in range(i+1):
+#         print("*",end="")
+
+#     print() 
--- Practice/Practice.py
+++ Practice/Practice.py
@@ -87,16 +87,16 @@
 #         print(j,end="\t")
 #     print()
 
-# n=3
-# for i in range (n,0,-1):
-#     for j in range (1,i+1):
-#         print("*",end=" ")
-#     print()
+    # n=3
+    # for i in range (n,0,-1):
+    #     for j in range (1,i+1):
+    #         print("*",end=" ")
+    #     print()
 
-n=3
-for i in range (1,n+1):
-    for k in range (n-1):
-        print(" ",end=" ")
-for j in range (1,i+1):
-    print("*",end=" ")
-print()
+# n=3
+# for i in range (1,n+1):
+#     for k in range (n-1):
+#         print(" ",end=" ")
+# for j in range (1,i+1):
+#     print("*",end=" ")
+# print()
