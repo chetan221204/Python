f2350e7259c4c417160f0a9a1c130715ed9579c1
Chetan Chauhan <chetanchauhan247@gmail.com>
2026-06-02 09:14:39 +0530

    lcm

--- Demo/tempCodeRunnerFile.py
+++ Demo/tempCodeRunnerFile.py
@@ -56,6 +56,57 @@
 # print(odd)
 # print(product)
 
-a=121
-while a!=0:
+# a=121
+# y=a
+# u=0
+# while a!=0:
+#     x=a%10
+#     u=u*10+x
+#     a=a//10
     
+#     if (y==u):
+#         print("Its a palindrome ")
+
+
+# # factprial
+# n=int(input())
+# fact=1
+# for i in range(1,10+1):
+#     fact=n*i
+#     print(n,"*",i,"=",fact)
+
+
+# n=int (input())
+# a=0
+# b=1
+# print(" ",a," ",b,end="")
+# for i in range(1,n):
+#     c=a+b
+#     print(" ",c,end="")
+#     a=b
+#     b=c
+
+ 
+# Prime Number
+# a=int(input())
+# prime_number=True
+# for i in range(2,a):
+#     if (a%i==0):
+#         prime_number=False
+#         break
+# if(prime_number==False):
+#     print("It's Not a  Prime Number")
+# else:
+#     print("It's a Prime Number")
+
+
+
+# LCM
+n1,n2=map(int ,input().split())
+max_num=max(n1,n2)
+while True:
+    if(max_num%n1==0 and max_num%n2==0):
+        print("LCM Of",n1,"and",n2,"is",max_num)
+        break
+
+    max_num+=1
