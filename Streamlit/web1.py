a486b7fc5fb38588ac8a10b14072af7ab7595b2d
Chetan Chauhan <chetanchauhan247@gmail.com>
2026-06-16 09:55:14 +0530

    steamlit

---
+++ Streamlit/web1.py
@@ -0,0 +1,13 @@
+import streamlit as st
+# st.title("My First Streamlit App")
+# st.header("Welcome to Streanlit!")
+# st.write("copyright")
+
+st.title("This is a Title")
+st.header("This is a header")
+st.subheader("This is a subheader")
+st.write("This is a subheader")
+st.markdown("**This is markdown**")
+st.text("This is plain text")
+st.code("print('Hello World')")
+st.latex(r"\frac{1}{2}")
